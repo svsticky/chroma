34cd68c83810dd8ac9ed202b123f15200ae72feb
Mervin de Jong <mervin.dj@hotmail.com>
2024-07-30 14:58:57 +0200

    Replaced process::exit() call with a return statement returning an ExitCode

--- server/chroma/src/main.rs
+++ server/chroma/src/main.rs
@@ -1,5 +1,7 @@
 extern crate core;
 
+use std::process::ExitCode;
+
 use dotenv::dotenv;
 use tracing::level_filters::LevelFilter;
 use tracing::{error, warn};
@@ -13,7 +15,7 @@ mod routes;
 mod server;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> ExitCode {
     // Try to load the environment variables from the .env file
     let dotenv_err = dotenv().err();
 
@@ -31,7 +33,9 @@ async fn main() {
         err.chain()
             .skip(1)
             .for_each(|cause| error!("because: {}", cause));
-        std::process::exit(1);
+        ExitCode::from(1)
+    } else {
+        ExitCode::from(0)
     }
 }
 
